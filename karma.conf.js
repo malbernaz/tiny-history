df63150738110f484559bd0f3d4e547781d66dd5
malbernaz <miguel.albernaz@concrete.com.br>
2017-08-04 15:17:01 -0300

    add ci

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,51 @@
 const webpack = require("webpack");
 
 module.exports = config => {
+  const customLaunchers = {
+    SL_Chrome: {
+      base: "SauceLabs",
+      platform: "Windows 10",
+      browserName: "chrome",
+      version: "47.0"
+    },
+    SL_Firefox: {
+      base: "SauceLabs",
+      platform: "Windows 10",
+      browserName: "firefox",
+      version: "43.0"
+    },
+    SL_Safari: {
+      base: "SauceLabs",
+      platform: "OS X 10.11",
+      browserName: "safari",
+      version: "9.0"
+    },
+    SL_MobileSafari8: {
+      base: "SauceLabs",
+      platform: "OS X 10.11",
+      browserName: "iphone",
+      version: "7.1"
+    },
+    SL_MobileSafari9: {
+      base: "SauceLabs",
+      platform: "ios",
+      os_version: "9.1",
+      browserName: "iphone"
+    },
+    SL_InternetExplorer10: {
+      base: "SauceLabs",
+      platform: "Windows 8.1",
+      browserName: "ie",
+      version: "10.0"
+    },
+    SL_AndroidBrowser: {
+      base: "SauceLabs",
+      browserName: "Browser",
+      platform: "Android",
+      version: "4.3"
+    }
+  };
+
   config.set({
     browsers: ["ChromeHeadless"],
     frameworks: ["mocha"],
@@ -26,4 +71,12 @@ module.exports = config => {
       noInfo: true
     }
   });
+
+  if (process.env.TRAVIS) {
+    config.browsers = Object.keys(customLaunchers);
+    config.reporters = ["dots", "saucelabs"];
+    config.sauceLabs = {
+      testName: "Web App Unit Tests"
+    };
+  }
 };
