107c8de45a5dd3744adc77172c914bca59473ae7
malbernaz <miguel.albernaz@concrete.com.br>
2017-08-04 18:03:10 -0300

    updates

--- karma.conf.js
+++ karma.conf.js
@@ -1,48 +1,50 @@
 const webpack = require("webpack");
 
 module.exports = config => {
-  const customLaunchers = {
-    SL_Chrome: {
-      base: "SauceLabs",
-      platform: "Windows 10",
-      browserName: "chrome",
+  const customlaunchers = {
+    sl_chrome: {
+      base: "saucelabs",
+      platform: "windows 10",
+      browsername: "chrome",
       version: "47.0"
     },
-    SL_Firefox: {
-      base: "SauceLabs",
-      platform: "Windows 10",
-      browserName: "firefox",
+    sl_firefox: {
+      base: "saucelabs",
+      platform: "windows 10",
+      browsername: "firefox",
       version: "43.0"
     },
-    SL_Safari: {
-      base: "SauceLabs",
-      platform: "OS X 10.11",
-      browserName: "safari",
+    sl_safari: {
+      base: "saucelabs",
+      platform: "os x 10.11",
+      browsername: "safari",
       version: "9.0"
     },
-    SL_MobileSafari8: {
-      base: "SauceLabs",
-      platform: "OS X 10.11",
-      browserName: "iphone",
+    sl_mobilesafari8: {
+      base: "saucelabs",
+      platform: "os x 10.11",
+      browsername: "iphone",
       version: "7.1"
     },
-    SL_MobileSafari9: {
-      base: "SauceLabs",
+    sl_mobilesafari9: {
+      base: "saucelabs",
       platform: "ios",
-      os_version: "9.1",
-      browserName: "iphone"
+      browsername: "safari",
+      version: "8.1",
+      devicename: "iphone emulator"
     },
-    SL_InternetExplorer10: {
-      base: "SauceLabs",
-      platform: "Windows 8.1",
-      browserName: "ie",
+    sl_internetexplorer10: {
+      base: "saucelabs",
+      platform: "windows 8",
+      browsername: "internet explorer",
       version: "10.0"
     },
-    SL_AndroidBrowser: {
-      base: "SauceLabs",
-      browserName: "Browser",
-      platform: "Android",
-      version: "4.3"
+    sl_androidbrowser: {
+      base: "saucelabs",
+      browsername: "browser",
+      platform: "android",
+      devicename: "android emulator",
+      version: "4.4"
     }
   };
 
