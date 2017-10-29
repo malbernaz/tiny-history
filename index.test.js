5414f400322c7739067ed84e3e5a8622843b0df9
malbernaz <albernazmiguel@gmail.com>
2017-10-29 16:00:32 -0200

    updates

--- index.test.js
+++ index.test.js
@@ -17,281 +17,276 @@ function sleep(time = 100) {
 }
 
 describe("history", () => {
+  let history;
   beforeEach(() => {
     window.history.replaceState(null, null, "/");
+    history = createHistory();
   });
 
-  describe("default behaviour", () => {
-    let history;
-    beforeEach(() => {
-      history = createHistory();
+  describe("listen", () => {
+    it("does not immediately call listeners", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
+
+      expect(listener.calls.length).toBe(0);
+
+      unlisten();
     });
+  });
 
-    describe("listen", () => {
-      it("does not immediately call listeners", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
+  describe("the initial location", () => {
+    it("does not have a key", () => {
+      expect(history.location.key).toNotExist();
+    });
+  });
 
-        expect(listener.calls.length).toBe(0);
+  describe("push a new path", () => {
+    it("calls change listeners with the new location", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
 
-        unlisten();
+      expect(history.location.pathname).toBe("/");
+
+      history.push("/home?the=query#the-hash");
+      const [location, action] = listener.calls[0].arguments;
+      expect(action).toBe("PUSH");
+      expect(location).toMatch({
+        pathname: "/home",
+        search: "?the=query",
+        hash: "#the-hash"
       });
+
+      unlisten();
     });
+  });
 
-    describe("the initial location", () => {
-      it("does not have a key", () => {
-        expect(history.location.key).toNotExist();
-      });
-    });
+  describe("push the same path", () => {
+    it("calls change listeners with the new location", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
 
-    describe("push a new path", () => {
-      it("calls change listeners with the new location", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
+      expect(history.location.pathname).toBe("/");
 
-        expect(history.location.pathname).toBe("/");
+      history.push("/home");
+      let [{ pathname }, action] = lastCallArgs(listener);
+      expect(action).toBe("PUSH");
+      expect(pathname).toBe("/home");
 
-        history.push("/home?the=query#the-hash");
-        const [location, action] = listener.calls[0].arguments;
-        expect(action).toBe("PUSH");
-        expect(location).toMatch({
-          pathname: "/home",
-          search: "?the=query",
-          hash: "#the-hash"
-        });
+      history.push("/home");
+      [{ pathname }, action] = lastCallArgs(listener);
+      expect(action).toBe("PUSH");
+      expect(pathname).toBe("/home");
 
-        unlisten();
-      });
-    });
+      history.back();
 
-    describe("push the same path", () => {
-      it("calls change listeners with the new location", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
-
-        expect(history.location.pathname).toBe("/");
-
-        history.push("/home");
-        let [{ pathname }, action] = lastCallArgs(listener);
-        expect(action).toBe("PUSH");
-        expect(pathname).toBe("/home");
-
-        history.push("/home");
+      return sleep().then(() => {
         [{ pathname }, action] = lastCallArgs(listener);
-        expect(action).toBe("PUSH");
+        expect(action).toBe("POP");
         expect(pathname).toBe("/home");
 
-        history.back();
+        unlisten();
+      });
+    });
+  });
 
-        return sleep().then(() => {
+  describe("push state", () => {
+    it("calls change listeners with the new location", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
+
+      expect(history.location.pathname).toBe("/");
+
+      history.push("/home?the=query#the-hash", { the: "state" });
+      const [location, action] = listener.calls[0].arguments;
+      expect(action).toBe("PUSH");
+      expect(location).toMatch({
+        pathname: "/home",
+        search: "?the=query",
+        hash: "#the-hash",
+        state: { the: "state" }
+      });
+
+      unlisten();
+    });
+  });
+
+  describe("push with no pathname", () => {
+    it("calls change listeners with the new location", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
+
+      expect(history.location.pathname).toBe("/");
+
+      history.push("/home?the=query#the-hash");
+      let [location, action] = lastCallArgs(listener);
+      expect(action).toBe("PUSH");
+      expect(location).toMatch({
+        pathname: "/home",
+        search: "?the=query",
+        hash: "#the-hash"
+      });
+
+      history.push("?another=query#another-hash");
+      [location, action] = lastCallArgs(listener);
+      expect(action).toBe("PUSH");
+      expect(location).toMatch({
+        pathname: "/home",
+        search: "?another=query",
+        hash: "#another-hash"
+      });
+
+      unlisten();
+    });
+  });
+
+  describe("replace a new path", () => {
+    it("calls change listeners with the new location", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
+
+      expect(history.location.pathname).toBe("/");
+
+      history.replace("/home?the=query#the-hash");
+      const [location, action] = listener.calls[0].arguments;
+      expect(action).toBe("REPLACE");
+      expect(location).toMatch({
+        pathname: "/home",
+        search: "?the=query",
+        hash: "#the-hash"
+      });
+
+      unlisten();
+    });
+  });
+
+  describe("replace the same path", () => {
+    it("calls change listeners with the new location", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
+
+      expect(history.location.pathname).toBe("/");
+
+      history.replace("/home");
+      let [{ pathname }, action] = lastCallArgs(listener);
+      expect(action).toBe("REPLACE");
+      expect(pathname).toBe("/home");
+
+      history.replace("/home");
+      [{ pathname }, action] = lastCallArgs(listener);
+      expect(action).toBe("REPLACE");
+      expect(pathname).toBe("/home");
+
+      unlisten();
+    });
+  });
+
+  describe("replace state", () => {
+    it("calls change listeners with the new location", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
+
+      expect(history.location.pathname).toBe("/");
+
+      history.replace("/home?the=query#the-hash", { the: "state" });
+      const [location, action] = listener.calls[0].arguments;
+      expect(action).toBe("REPLACE");
+      expect(location).toMatch({
+        pathname: "/home",
+        search: "?the=query",
+        hash: "#the-hash",
+        state: { the: "state" }
+      });
+
+      unlisten();
+    });
+  });
+
+  describe("go back", () => {
+    it("calls change listeners with the previous location", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
+
+      expect(history.location.pathname).toBe("/");
+
+      history.push("/home");
+      let [{ pathname }, action] = lastCallArgs(listener);
+      expect(action).toBe("PUSH");
+      expect(pathname).toBe("/home");
+
+      history.back();
+      return sleep().then(() => {
+        [{ pathname }, action] = lastCallArgs(listener);
+        expect(action).toBe("POP");
+        expect(pathname).toBe("/");
+
+        unlisten();
+      });
+    });
+  });
+
+  describe("go forward", () => {
+    it("calls change listeners with the next location", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
+
+      expect(history.location.pathname).toBe("/");
+
+      history.push("/home");
+      let [{ pathname }, action] = lastCallArgs(listener);
+      expect(action).toBe("PUSH");
+      expect(pathname).toBe("/home");
+
+      history.back();
+      return sleep()
+        .then(() => {
+          [{ pathname }, action] = lastCallArgs(listener);
+          expect(action).toBe("POP");
+          expect(pathname).toBe("/");
+
+          history.forward();
+
+          return sleep();
+        })
+        .then(() => {
           [{ pathname }, action] = lastCallArgs(listener);
           expect(action).toBe("POP");
           expect(pathname).toBe("/home");
 
           unlisten();
         });
-      });
     });
+  });
 
-    describe("push state", () => {
-      it("calls change listeners with the new location", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
+  describe("go to index", () => {
+    it("calls change listeners with the given an index of an entry", () => {
+      const listener = createSpy();
+      const unlisten = history.listen(listener);
 
-        expect(history.location.pathname).toBe("/");
+      expect(history.location.pathname).toBe("/");
 
-        history.push("/home?the=query#the-hash", { the: "state" });
-        const [location, action] = listener.calls[0].arguments;
-        expect(action).toBe("PUSH");
-        expect(location).toMatch({
-          pathname: "/home",
-          search: "?the=query",
-          hash: "#the-hash",
-          state: { the: "state" }
-        });
+      history.push("/home");
+      let [{ pathname }, action] = lastCallArgs(listener);
+      expect(action).toBe("PUSH");
+      expect(pathname).toBe("/home");
 
-        unlisten();
-      });
-    });
-
-    describe("push with no pathname", () => {
-      it("calls change listeners with the new location", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
-
-        expect(history.location.pathname).toBe("/");
-
-        history.push("/home?the=query#the-hash");
-        let [location, action] = lastCallArgs(listener);
-        expect(action).toBe("PUSH");
-        expect(location).toMatch({
-          pathname: "/home",
-          search: "?the=query",
-          hash: "#the-hash"
-        });
-
-        history.push("?another=query#another-hash");
-        [location, action] = lastCallArgs(listener);
-        expect(action).toBe("PUSH");
-        expect(location).toMatch({
-          pathname: "/home",
-          search: "?another=query",
-          hash: "#another-hash"
-        });
-
-        unlisten();
-      });
-    });
-
-    describe("replace a new path", () => {
-      it("calls change listeners with the new location", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
-
-        expect(history.location.pathname).toBe("/");
-
-        history.replace("/home?the=query#the-hash");
-        const [location, action] = listener.calls[0].arguments;
-        expect(action).toBe("REPLACE");
-        expect(location).toMatch({
-          pathname: "/home",
-          search: "?the=query",
-          hash: "#the-hash"
-        });
-
-        unlisten();
-      });
-    });
-
-    describe("replace the same path", () => {
-      it("calls change listeners with the new location", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
-
-        expect(history.location.pathname).toBe("/");
-
-        history.replace("/home");
-        let [{ pathname }, action] = lastCallArgs(listener);
-        expect(action).toBe("REPLACE");
-        expect(pathname).toBe("/home");
-
-        history.replace("/home");
-        [{ pathname }, action] = lastCallArgs(listener);
-        expect(action).toBe("REPLACE");
-        expect(pathname).toBe("/home");
-
-        unlisten();
-      });
-    });
-
-    describe("replace state", () => {
-      it("calls change listeners with the new location", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
-
-        expect(history.location.pathname).toBe("/");
-
-        history.replace("/home?the=query#the-hash", { the: "state" });
-        const [location, action] = listener.calls[0].arguments;
-        expect(action).toBe("REPLACE");
-        expect(location).toMatch({
-          pathname: "/home",
-          search: "?the=query",
-          hash: "#the-hash",
-          state: { the: "state" }
-        });
-
-        unlisten();
-      });
-    });
-
-    describe("go back", () => {
-      it("calls change listeners with the previous location", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
-
-        expect(history.location.pathname).toBe("/");
-
-        history.push("/home");
-        let [{ pathname }, action] = lastCallArgs(listener);
-        expect(action).toBe("PUSH");
-        expect(pathname).toBe("/home");
-
-        history.back();
-        return sleep().then(() => {
+      history.go(-1);
+      return sleep()
+        .then(() => {
           [{ pathname }, action] = lastCallArgs(listener);
           expect(action).toBe("POP");
           expect(pathname).toBe("/");
 
+          history.go(1);
+
+          return sleep();
+        })
+        .then(() => {
+          [{ pathname }, action] = lastCallArgs(listener);
+          expect(action).toBe("POP");
+          expect(pathname).toBe("/home");
+
           unlisten();
         });
-      });
-    });
-
-    describe("go forward", () => {
-      it("calls change listeners with the next location", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
-
-        expect(history.location.pathname).toBe("/");
-
-        history.push("/home");
-        let [{ pathname }, action] = lastCallArgs(listener);
-        expect(action).toBe("PUSH");
-        expect(pathname).toBe("/home");
-
-        history.back();
-        return sleep()
-          .then(() => {
-            [{ pathname }, action] = lastCallArgs(listener);
-            expect(action).toBe("POP");
-            expect(pathname).toBe("/");
-
-            history.forward();
-
-            return sleep();
-          })
-          .then(() => {
-            [{ pathname }, action] = lastCallArgs(listener);
-            expect(action).toBe("POP");
-            expect(pathname).toBe("/home");
-
-            unlisten();
-          });
-      });
-    });
-
-    describe("go to index", () => {
-      it("calls change listeners with the given an index of an entry", () => {
-        const listener = createSpy();
-        const unlisten = history.listen(listener);
-
-        expect(history.location.pathname).toBe("/");
-
-        history.push("/home");
-        let [{ pathname }, action] = lastCallArgs(listener);
-        expect(action).toBe("PUSH");
-        expect(pathname).toBe("/home");
-
-        history.go(-1);
-        return sleep()
-          .then(() => {
-            [{ pathname }, action] = lastCallArgs(listener);
-            expect(action).toBe("POP");
-            expect(pathname).toBe("/");
-
-            history.go(1);
-
-            return sleep();
-          })
-          .then(() => {
-            [{ pathname }, action] = lastCallArgs(listener);
-            expect(action).toBe("POP");
-            expect(pathname).toBe("/home");
-
-            unlisten();
-          });
-      });
     });
   });
 });
